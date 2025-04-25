e8a6dca87d67dddc7e1449ee88b761da07de2d28
Your Name <jetendrasinghratore1@gmail.com>
2025-04-25 18:09:16 +0530

    sixth commit

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -113,7 +113,7 @@ const Footer = () => {
                       <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                     </svg>
                     <span>
-                      Haripura, Upadhyay Nagar, C-Block, <br/>
+                      Haribhau, Upadhyay Nagar, C-Block, <br/>
                       Pushkar Road, Ajmer, Rajasthan
                     </span>
                   </p>
